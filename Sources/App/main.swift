fb8d128768fb87de6851e8fb6eac83f131a6838e
riizu <rgbatty@outlook.com>
2016-09-10 19:37:09 -0600

    add initial routes

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -8,14 +8,21 @@ drop.get("/") { request in
     return try drop.view.make("index")
 }
 
+drop.get("faq") { request in
+    return try drop.view.make("faq")
+}
+
+drop.get("apply") { request in
+    return try drop.view.make("apply")
+}
+
+drop.get("login") { request in
+    return try drop.view.make("login")
+}
+
 let users = UserController(droplet: drop)
 drop.resource("users", users)
 
-drop.get("leaf") { request in
-    return try drop.view.make("template", [
-        "greeting": "Hello, world!"
-    ])
-}
 
 drop.middleware.append(SampleMiddleware())
 
